12f917f88dad99aae56c84d7af9aa8aeb027d774
DESKTOP-50NP26A\Henry <spookyboo@hetnet.nl>
2023-03-08 17:00:48 +0100

    Added 2 tests; one default and one release build

--- src/test/java/PipelineUnit.java
+++ src/test/java/PipelineUnit.java
@@ -20,23 +20,42 @@ public class PipelineUnit {
         // Initialize the pipeline (resource path is default)
         pipeline = new AzDoPipeline("hello-pipeline-my.properties", "./pipeline/pipeline.yml");
 
-        // Add commands to the bundle. These commands are executed for every test, so you only have to do it once
-        // The pipeline may not fail because org.pipeline:junit-pipeline:jar cannot be found
-        // So, ignore all unit tests, because we also don't test the app anyway
-        pipeline.commandBundle.overrideLiteral("clean install", "clean install -Dmaven.test.skip=true", true);
-
         // Remove the dependency to artifact 'junit-pipeline' before it is deployed to the AzDo test project
         pipeline.deleteDependencyFromTargetPom("org.pipeline", "junit-pipeline");
+
+        /*
+            Add commands to the bundle. These commands are executed for every test, so you only have to do it once.
+            The pipeline may not fail because org.pipeline:junit-pipeline:jar cannot be found (it is deleted from the pom.xml).
+            The pipeline may also not fails because the PipelineUnit.java cannot be compiled (because the org.pipeline:junit-pipeline:jar was removed).
+            So, ignore all unit tests, because we also don't test the app anyway.
+         */
+        pipeline.commandBundle.overrideLiteral("clean install", "clean install -Dmaven.test.skip=true", true);
     }
 
     @Test
-    public void test1() {
+    public void testDefaultBuild() {
         logger.info("");
         logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        logger.info("Perform unittest: test1");
+        logger.info("Perform unittest: Test default build");
         logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-        // Manipulate the pipeline
+        try {
+            pipeline.startPipeline();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertEquals (RunResult.Result.succeeded, pipeline.getRunResult().result);
+    }
+
+    @Test
+    public void testReleaseBuild() {
+        logger.info("");
+        logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        logger.info("Perform unittest: Test release build");
+        logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        // Assign the release version to the parameter
         pipeline.overrideParameterDefault("releaseVersion", "1.0.0");
 
         try {
